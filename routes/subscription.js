546b0f86d83f9f85800fba5ed55fe3f9a769651f
Saugata Halder <saugata1990@gmail.com>
2019-03-30 16:47:51 +0530

    code changes

--- routes/subscription.js
+++ routes/subscription.js
@@ -12,18 +12,20 @@ subscription.post('/', (req, res) => {
     let payload = JSON.stringify({title: 'Friends League', body: 'Here is the notification'})
     const schedule = require('node-schedule')
     let match1 = null, match2 = null
-
+    const now = new Date()
     
 
     // might merge the following 2 blocks into one block
     schedule.scheduleJob('30 9 * * *', () => { 
         Match.find().exec()
         .then(matches => {
-            match1 = matches.find(match => date.subtract(match.start_time, new Date()).toHours() <= 1)
+            match1 = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
+             && date.subtract(match.start_time, now).toHours() >= 0)
             if(match1){
                 console.log(match1)
                 const message = 
-                `Match #${match1.match_no}: ${match1.team1} vs ${match1.team2} is coming up in less than an hour. Have you posted your prediction yet?`
+                `Match #${match1.match_no}: ${match1.team1} vs ${match1.team2} is coming up in less than an hour.
+                 Have you posted your prediction yet?`
                 payload = JSON.stringify({title: 'Friends League', body: message})
                 webpush.sendNotification(subscription, payload).catch(error => console.log(error))
             }  
@@ -34,10 +36,12 @@ subscription.post('/', (req, res) => {
     schedule.scheduleJob('30 14 * * *', () => { 
         Match.find().exec()
         .then(matches => {
-            match2 = matches.find(match => date.subtract(match.start_time, new Date()).toHours() <= 1)
+            match2 = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
+             && date.subtract(match.start_time, now).toHours() >= 0)
             if(match2){
                 const message = 
-                `Match #${match2.match_no}: ${match2.team1} vs ${match2.team2} is coming up in less than an hour. Have you posted your prediction yet?`
+                `Match #${match2.match_no}: ${match2.team1} vs ${match2.team2} is coming up in less than an hour.
+                 Have you posted your prediction yet?`
                 payload = JSON.stringify({title: 'Friends League', body: message})
                 webpush.sendNotification(subscription, payload).catch(error => console.log(error))
             }  
