f4d17b420cf1be5f64fe9a5f2a955aae8c505c16
Saugata Halder <saugata1990@gmail.com>
2019-03-30 16:49:53 +0530

    code changes

--- routes/subscription.js
+++ routes/subscription.js
@@ -17,12 +17,12 @@ subscription.post('/', (req, res) => {
 
     // might merge the following 2 blocks into one block
     schedule.scheduleJob('30 9 * * *', () => { 
+        console.log('Scheduled job is running')
         Match.find().exec()
         .then(matches => {
             match1 = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
              && date.subtract(match.start_time, now).toHours() >= 0)
             if(match1){
-                console.log(match1)
                 const message = 
                 `Match #${match1.match_no}: ${match1.team1} vs ${match1.team2} is coming up in less than an hour.
                  Have you posted your prediction yet?`
@@ -34,6 +34,7 @@ subscription.post('/', (req, res) => {
 
 
     schedule.scheduleJob('30 14 * * *', () => { 
+        console.log('Scheduled job is running')
         Match.find().exec()
         .then(matches => {
             match2 = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
