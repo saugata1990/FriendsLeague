7e5276c2aca433965e1da002312160bc4a34643a
Saugata Halder <saugata1990@gmail.com>
2019-03-31 12:04:03 +0530

    code changes

--- routes/subscription.js
+++ routes/subscription.js
@@ -11,50 +11,29 @@ subscription.post('/', (req, res) => {
     res.status(201).json({})
     let payload = JSON.stringify({title: 'Friends League', body: 'Here is the notification'})
     const schedule = require('node-schedule')
-    let match1 = null, match2 = null
     const now = new Date()
-   
-    
-    // test
-
-    // test scheduling on heroku
-    schedule.scheduleJob('25 5 * * *', () => {
-        payload = JSON.stringify({title: 'Friends League', body: 'Testing notifications'})
-        webpush.sendNotification(subscription, payload).catch(error => console.log(error))
-    })
-
-    // might merge the following 2 blocks into one block
-    schedule.scheduleJob('30 9 * * *', () => { 
-        console.log('Scheduled job is running')
-        Match.find().exec()
-        .then(matches => {
-            match1 = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
-             && date.subtract(match.start_time, now).toHours() >= 0)
-            if(match1){
-                const message = 
-                `Match #${match1.match_no}: ${match1.team1} vs ${match1.team2} is coming up in less than an hour.
-                 Have you posted your prediction yet?`
-                payload = JSON.stringify({title: 'Friends League', body: message})
-                webpush.sendNotification(subscription, payload).catch(error => console.log(error))
-            }  
-        })         
-    })
-
-
-    schedule.scheduleJob('30 14 * * *', () => { 
-        console.log('Scheduled job is running')
-        Match.find().exec()
-        .then(matches => {
-            match2 = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
-             && date.subtract(match.start_time, now).toHours() >= 0)
-            if(match2){
-                const message = 
-                `Match #${match2.match_no}: ${match2.team1} vs ${match2.team2} is coming up in less than an hour.
-                 Have you posted your prediction yet?`
-                payload = JSON.stringify({title: 'Friends League', body: message})
-                webpush.sendNotification(subscription, payload).catch(error => console.log(error))
-            }  
-        })         
+    const timeslots = ['30 9 * * *', '30 14 * * *']
+    // const timeslots = ['58 11 * * *', '59 11 * * *']
+    timeslots.map(slot => {
+        schedule.scheduleJob(slot, () => {  
+            console.log('*********Scheduled job is running***********')
+            Match.find().exec()
+            .then(matches => {
+                const match = matches.find(match => date.subtract(match.start_time, now).toHours() <= 1
+                 && date.subtract(match.start_time, now).toHours() >= 0)
+                if(match){
+                    console.log(`*******Found Match ${match}*********`)
+                    const message = 
+                    `Match #${match.match_no}: ${match.team1} vs ${match.team2} is coming up in less than an hour. Have you posted your prediction yet?`
+                    payload = JSON.stringify({title: 'Friends League', body: message})
+                    webpush.sendNotification(subscription, payload).catch(error => console.log(error))
+                } 
+                // this part is for testing in production, and will be removed
+                else{
+                    console.log('********No match found**********')
+                } 
+            })         
+        })
     })
 })
 
