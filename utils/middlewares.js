25fb5d6f6ad387af80de14f52510616e11dcc113
Saugata Halder <saugata1990@gmail.com>
2019-04-02 21:08:16 +0530

    code changes

--- utils/middlewares.js
+++ utils/middlewares.js
@@ -10,7 +10,7 @@ const isLoggedIn = (req, res, next) => {
         next();
     }
     else{
-        res.render('error', {user: 'user'})
+        res.render('error', {user: 'playerrrrrrrrr'})
     }
 }
 
